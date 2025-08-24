fe27c5a5c9e94859cb292a6cefb1be54939095aa
Godawela <godawelaus.21@uom.lk>
2025-08-24 15:40:15 +0530

    sysmptom image upload

--- routes/symptomRoutes.js
+++ routes/symptomRoutes.js
@@ -4,11 +4,12 @@ const symptomController = require('../controllers/symptomController');
 const router = express.Router();
 
 // CRUD routes for symptoms
-router.post('/symptoms', symptomController.createSymptom);
+router.post('/', upload.single('image'), symptomController.createSymptom, handleMulterError);
+router.patch('/:id', upload.single('image'), symptomController.updateSymptom, handleMulterError);
+router.delete('/:id', symptomController.deleteSymptom);
 router.get('/symptoms', symptomController.getSymptoms);
 router.get('/symptoms/name/:name', symptomController.getSymptomByName);
 router.get('/symptoms/:id', symptomController.getSymptomById);
-router.patch('/symptoms/:id', symptomController.updateSymptom);
-router.delete('/symptoms/:id', symptomController.deleteSymptom);
+
 
 module.exports = router;
