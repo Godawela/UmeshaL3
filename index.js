579f1b8af7175a8f34d6c72c5bd42059aa516398
Godawela <godawelaus.21@uom.lk>
2025-08-27 14:55:30 +0530

    remove cors

--- index.js
+++ index.js
@@ -16,8 +16,6 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const cors = require('cors');
-app.use(cors());
 
 
 app.use(express.urlencoded({ extended: true }));
