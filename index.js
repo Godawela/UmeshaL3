421b7261022abf3f188807539977afa673fbb876
Godawela <godawelaus.21@uom.lk>
2025-08-27 14:48:13 +0530

    add cros

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
+app.use(cors());
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
