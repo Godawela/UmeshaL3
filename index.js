da10c21705002bf9f91a081e6d23350da611cd1d
Godawela <godawelaus.21@uom.lk>
2025-08-28 19:56:05 +0530

    change to env variables

--- index.js
+++ index.js
@@ -13,9 +13,12 @@ const questionRoutes = require('./routes/questionRoutes'); // Correct path for q
 const admin = require('firebase-admin');
 
 // Initialize Firebase Admin
-const serviceAccount = require('./config/service-account-key.json'); // Update path
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
 });
 
 
